5bcd5201aea4925dbfcd46e11b91cd450b775259
Zinhle Mtimunye <88083780+zinhlenelakhe@users.noreply.github.com>
2022-01-26 17:24:17 +0000

    Added lines that prompts the user

--- doWhile.cpp
+++ doWhile.cpp
@@ -3,7 +3,13 @@ using namespace std;
 
 int main()
 {
+     int evenNum;
      int i = 0;
      do
      {
+          cout<<"Enter even number\n";
+          cin>>evenNum;
+          
+          
           cout 
+      }while(evenNum < 5);        
