0762078b58ed430a86175316024435619c53b13c
Ghaven <97505123+GhavenCode@users.noreply.github.com>
2022-01-28 08:23:23 +0200

    Adding a capitalizer code

--- name.cpp
+++ name.cpp
@@ -1,12 +1,39 @@
 #include<iostream>
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
 using namespace std;
 
 int main()
 {
- string name;
+char name[30];
  
  cout << " Enter your name " << endl;
  cin >> name;
+ 
+ int length = strlen(name);
+ for(int i=0;i<length;i++)
+    {
+        if(i==0)
+        {
+            if(islower(name[i]))
+            name[i]=toupper(name[i]);
+        }
+        else
+        {
+            if(name[i]!=' ')
+            {
+                if(isupper(name[i]))
+                    name[i]=tolower(name[i]);
+            }
+            else
+            {
+                i++;
+                if(islower(name[i]))
+                    name[i]=toupper(name[i]);
+            }
+        }
+    }
  
  cout << " Hello " << name << "!" << endl;
 
